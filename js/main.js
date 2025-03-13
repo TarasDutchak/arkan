5925c51c59017e930ad73c471b12b949106accb9
TarasDutchak <Tarasdutchak90@gmail.com>
2025-03-13 18:54:02 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,25 +9,11 @@ $(document).ready(function () {
     });
 
     // home page slider
-    // var swiper = new Swiper(".infoslider", {
-    //     slidesPerView: "auto",
-    //     spaceBetween: 21,
-    //     // loop: true,
-    //     // centeredSlides: true,
-    //     initialSlide: 1,
-    // });
-
-
-    $('.infoslider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true,
-        arrows: false,
-        dots: false,
-        initialSlide: 2
+    var swiper2 = new Swiper(".infoslider", {
+        slidesPerView: "auto",
+        spaceBetween: 21,
+        loop: true,
+        slidesOffsetBefore: -200
     });
 
     // parallax
@@ -36,16 +22,98 @@ $(document).ready(function () {
 
     // partners slider
 
-    $('.partners-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true,
-        arrows: false,
-        dots: false,
-        initialSlide: 2
+    var swiper = new Swiper(".partners-slider", {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        loop: true,
+        pagination: {
+            el: ".partners-slider .swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
+
+    // tabs - mainpage
+
+    $('.ourfacilities__tabbtns button').click(function () {
+        $('.ourfacilities__tabbtns button').removeClass('active');
+        $(this).addClass('active');
+    })
+
+    $('.btnswrap button').click(function () {
+        let index = $(this).parent().index();
+
+        $('.btnswrap button').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab-1, .tab-2, .tab-3').hide();
+        $('.tab-' + (index + 1)).show();
+    });
+
+    $('.btnswrap li .active').click();
+
+
+    // tabs sliders
+
+    var swiper = new Swiper(".ourfacilities__sl", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        speed: 1200,
+        initialSlide: 2,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var subSwiper = new Swiper(".hotprorsubsl", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        speed: 700,
+        watchSlidesProgress: true,
+        pagination: {
+            el: ".swiper-pagination.v3",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next.v3",
+            prevEl: ".swiper-button-prev.v3",
+        },
+    });
+
+    var mainSwiper = new Swiper(".hotprormainsl", {
+        slidesPerView: 1,
+        loop: true,
+        speed: 700,
+        navigation: {
+            nextEl: ".swiper-button-next.v3",
+            prevEl: ".swiper-button-prev.v3",
+        },
+        thumbs: {
+            swiper: subSwiper,
+        },
+        pagination: {
+            el: ".swiper-pagination.v3",
+            clickable: true,
+        },
+    });
+
+    // faq
+    $('.faqrow__header').click(function(){
+        $(this).parent().toggleClass('active');
+
+        $(this).next('.faqrow__body').slideToggle();
+    });
+
 
 });
